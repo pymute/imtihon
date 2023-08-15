4b23feb7037099cc9356529de1221340383fdc07
pymute <134302465+pymute@users.noreply.github.com>
2023-08-15 18:10:00 +0500

    Update serializers.py

--- kitob/serializers.py
+++ kitob/serializers.py
@@ -5,3 +5,8 @@ class Serializer(ModelSerializer):
     class Meta:
         model = avtorModel
         fields = ('id','kitob_nomi','janri','sifati','avtori','yili')
+
+class AvtorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = avtorModel
+        fields = ('kitob_id','avtor_ismi','avtor_familiyasi','kitoblari','tugulgan_sana')
