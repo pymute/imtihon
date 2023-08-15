264f52d2541f8a656ced7bdf6542922daf4fe0ad
pymute <134302465+pymute@users.noreply.github.com>
2023-08-15 18:04:39 +0500

    Update models.py

--- kitob/models.py
+++ kitob/models.py
@@ -9,14 +9,18 @@ class avtorModel(models.Model):
     sifati = models.CharField(max_length=13, default='')
     avtor = models.ForeignKey(on_delete=models.CASCADE)
     yili = models.DateField(default='')
-
-# class avtoridModel(models.Model):
-#     avtor_ismi = models.CharField(default='', max_length=15)
-#     kitoblari = models.CharField(default='', max_length=15)
-#     avtor_familiyasi = models.CharField(max_length=13, default='')
-#     tugulgan_sana = models.DateField(default='')
     def __str__(self) -> str:
         return self.kitob_nomi
+    class Meta:
+        db_table = 'kitob'
+        
+class avtoridModel(models.Model):
+    avtor_ismi = models.CharField(default='', max_length=15)
+    kitoblari = models.CharField(default='', max_length=15)
+    avtor_familiyasi = models.CharField(max_length=13, default='')
+    tugulgan_sana = models.DateField(default='')
+    def __str__(self) -> str:
+        return self.avtor_ismi
 
     class Meta:
         db_table = 'avtor'
